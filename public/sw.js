c9e049a167d9fd89a3764c6d7664a4052c326560
Igor <musichound.ge@gmail.com>
2025-11-12 22:58:02 +0500

    add pwa

---
+++ public/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'step-viewer-cache-v1'
+const PRECACHE_URLS = ['index.html', 'fav.png', 'manifest.webmanifest'].map((asset) =>
+  new URL(asset, self.registration.scope).toString(),
+)
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS)).then(() => self.skipWaiting()),
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse
+      }
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          const clonedResponse = networkResponse.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clonedResponse))
+          return networkResponse
+        })
+        .catch(() => cachedResponse)
+    }),
+  )
+})
