af39b0a69b2507cffb3e07bf50e7c07859daa22f
Igor <musichound.ge@gmail.com>
2025-11-13 14:16:43 +0500

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'step-viewer-cache-v2'
+const CACHE_NAME = 'step-viewer-cache-v3'
 const PRECACHE_URLS = ['index.html', 'fav.png', 'manifest.webmanifest'].map((asset) =>
   new URL(asset, self.registration.scope).toString(),
 )
